bee64669c275435a76dd6c02f392206d039a5b4d
jordiabante <jordi.abante@tamu.edu>
2015-06-26 19:20:47 -0500

    Improved example with more than one run

--- src/detonatePE/main/main.sh
+++ src/detonatePE/main/main.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -x
 
-detonatePE.sh -e -t 2 -- reads/reads_r1.fastq.gz reads/reads_r2.fastq.gz assembly/trinityOut.fasta 
+detonatePE.sh -e -t 2 -- reads/reads1_r1.fastq.gz reads/reads1_r2.fastq.gz assembly/trinityOut1.fasta 
+detonatePE.sh -e -t 2 -- reads/reads2_r1.fastq.gz reads/reads2_r2.fastq.gz assembly/trinityOut2.fasta 
--- src/seecerPE/main/main.sh
+++ src/seecerPE/main/main.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
-seecerPE.sh -k 20 -- reads/read_r1.fastq.gz reads/read_r2.fastq.gz
+seecerPE.sh -k 20 -- reads/read1_r1.fastq.gz reads/read1_r2.fastq.gz
+seecerPE.sh -k 20 -- reads/read2_r1.fastq.gz reads/read2_r2.fastq.gz
