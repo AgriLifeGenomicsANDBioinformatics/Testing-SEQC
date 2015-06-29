ff57bf7ee95570b2b1cef38d7718a2dd78482f6c
jordiabante <jordi.abante@tamu.edu>
2015-06-29 12:41:32 -0500

    Improved example

--- src/rsemEvalPE/main/main.sh
+++ src/rsemEvalPE/main/main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
 
-rsemEvalPE.sh -t 2 -- reads/reads1_r1.fastq.gz reads/reads1_r2.fastq.gz assembly/trinityOut1.fasta 
+rsemEvalPE.sh -t 2 -- reads/reads1_r1.fastq reads/reads1_r2.fastq assembly/trinityOut1.fasta 
 rsemEvalPE.sh -t 2 -- reads/reads2_r1.fastq.gz reads/reads2_r2.fastq.gz assembly/trinityOut2.fasta 
