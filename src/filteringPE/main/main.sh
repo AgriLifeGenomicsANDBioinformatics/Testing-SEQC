4d471130dbc19fbaf7ab3e4ebb1084a71374a494
jordiabante <jordi.abante@tamu.edu>
2015-06-29 12:57:38 -0500

    Files in Master branch

--- src/filteringPE/main/main.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-filteringPE.sh i1.8 2 reads/read_R1.fastq.gz reads/read_R2.fastq.gz
--- src/seecerPE/main/main.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-seecerPE.sh -k 20 -- reads/read1_r1.fastq reads/read1_r2.fastq
-seecerPE.sh -k 20 -- reads/read2_r1.fastq.gz reads/read2_r2.fastq.gz
-
-gunzip ./reads/read1*
