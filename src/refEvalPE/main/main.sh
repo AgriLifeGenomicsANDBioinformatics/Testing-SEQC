273a7deea5ee71d114d30f628fa68a253258cf03
jordiabante <jordi.abante@tamu.edu>
2015-07-01 13:32:30 -0500

    refEvalPE moved to master

--- src/refEvalPE/main/main.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-outdir="refEvalPEOut"
-
-if [ -d "$outdir" ]
-then
-  rm -rf "$outdir"
-fi
-
-refEvalPE.sh -- assemblies/toy_assembly_1.fa reference/toy_ref.fa reads/toy_SE.fq reads/toy_SE.fq "$outdir"
