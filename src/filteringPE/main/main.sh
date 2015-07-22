2853435b32418a0e94ea8a6f42880292fe8b99fb
jordiabante <jordi.abante@tamu.edu>
2015-07-22 11:35:45 -0500

    Included example with SEQC names

--- src/filteringPE/main/main.sh
+++ src/filteringPE/main/main.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 rm -rf filteringPE*
+set -x
 
-filteringPE.sh i1.8 2 reads/read_R1.fastq.gz reads/read_R2.fastq.gz filteringPEOut
+filteringPE.sh i1.8 2 reads/SEQC_ILM_AGR_A_1_L01_ATCACG_AC0C1TACXX_R1.fastq.gz \
+  reads/SEQC_ILM_AGR_A_1_L01_ATCACG_AC0C1TACXX_R2.fastq.gz \
+  filteringPEOut
+filteringPE.sh i1.8 2 reads/SEQC_ILM_AGR_A_1_L01_ATCACG_BD0J2JACXX_R1.fastq.gz \
+  reads/SEQC_ILM_AGR_A_1_L01_ATCACG_BD0J2JACXX_R2.fastq.gz \
+  filteringPEOut
