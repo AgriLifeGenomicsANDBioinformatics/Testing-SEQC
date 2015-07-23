00042adde77a90046e27a6712e66bdd4f75aac70
jordiabante <jordi.abante@tamu.edu>
2015-07-23 11:05:34 -0500

    Tried parallelized run

--- src/seecerPE/main/main.sh
+++ src/seecerPE/main/main.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-set -x
 
-seecerPE.sh -k 20 -- reads/read1_r1.fastq reads/read1_r2.fastq seecerPEOut1
-seecerPE.sh -k 20 -- reads/read2_r1.fastq.gz reads/read2_r2.fastq.gz seecerPEOut2
+seecerPE.sh -k 20 -- reads/read1_r1.fastq reads/read1_r2.fastq seecerPEOut &
+seecerPE.sh -k 20 -- reads/read2_r1.fastq.gz reads/read2_r2.fastq.gz seecerPEOut
 
 gunzip ./reads/read1*
